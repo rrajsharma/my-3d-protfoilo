7df4165844962359660d7784bd76251d34a9f66f
webdev-raj <codewithsapnap@gmail.com>
2025-07-19 18:47:18 +0530

    first commit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,69 +1,48 @@
-import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import { MdArrowOutward } from "react-icons/md";
 import "./styles/Contact.css";
 
 const Contact = () => {
   return (
-    <div className="contact-section section-container" id="contact">
-      <div className="contact-container">
-        <h3>Contact</h3>
-        <div className="contact-flex">
-          <div className="contact-box">
-            <h4>Email</h4>
-            <p>
-              <a href="mailto:example@mail.com" data-cursor="disable">
-                rrajsharma180@gmail.com
-              </a>
-            </p>
-            <h4>Phone</h4>
-            <p>
-              <a href="tel:+919920782622" data-cursor="disable">
-                +91 7879673504
-              </a>
-            </p>
-          </div>
-          <div className="contact-box">
-            <h4>Social</h4>
-            <a
+    <div className="background-container">
+      <div className="background-blur blur-1"></div>
+      <div className="background-blur blur-2"></div>
+
+      <div className="footer-content">
+        <h1 className="footer-heading">Creative Dev</h1>
+        <hr className="footer-divider" />
+        <div className="footer-links">
+          <h1><a
               href="https://github.com/rrajsharma"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
             >
               Github <MdArrowOutward />
-            </a>
-            <a
+            </a></h1>
+          <h1><a
               href="https://www.linkedin.com/in/raj-sharma8817/"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
             >
               Linkedin <MdArrowOutward />
-            </a>
-            <a
+            </a></h1>
+          <h1><a
               href="https://x.com/code_with_raj_8"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
             >
               Twitter <MdArrowOutward />
-            </a>
-            <a
+            </a></h1>
+          <h1><a
               href="https://www.instagram.com/code_with_raj_8/"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
             >
               Instagram <MdArrowOutward />
-            </a>
-          </div>
-          <div className="contact-box">
-            <h2>
-              Designed and Developed <br /> by <span>RAJ SHARMA</span>
-            </h2>
-            <h5>
-              <MdCopyright /> 2025
-            </h5>
-          </div>
+            </a></h1>
         </div>
       </div>
     </div>
